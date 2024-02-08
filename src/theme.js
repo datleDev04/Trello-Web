8e6feb0771be2857fa565b756e0ca74f507f70ee
letiendat040904@gmail.com <letiendat040904@gmail.com>
2024-02-09 01:21:29 +0700

    finised refactor structure of project

--- src/theme.js
+++ src/theme.js
@@ -6,14 +6,17 @@ import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 const APP_BAR_HEIGHT = '52px'
 const BOARD_BAR_HEIGHT = '56px'
 const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
-
+const COLLUMN_HEADER_HEIGHT = '52px'
+const COLLUMN_FOOTER_HEIGHT = '56px'
 
 // Create a theme instance.
 const theme = extendTheme({
   trello: {
     appBarHeight: APP_BAR_HEIGHT,
     boardBarHeight: BOARD_BAR_HEIGHT,
-    boardContentHeight: BOARD_CONTENT_HEIGHT
+    boardContentHeight: BOARD_CONTENT_HEIGHT,
+    collumnHeaderHeight : COLLUMN_HEADER_HEIGHT,
+    collumnFooterHeight : COLLUMN_FOOTER_HEIGHT
   },
   colorSchemes: {
     // light: {
