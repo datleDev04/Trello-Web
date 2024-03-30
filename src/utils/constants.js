2ced7fda77a5056b0de616b99d6e05ddbd3a194a
letiendat040904@gmail.com <letiendat040904@gmail.com>
2024-03-30 08:42:10 +0700

    deploy-updateAPIROOT

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
 
 let apiRoot = ''
 
-if (process.env.BUILD_MODE === 'production') {
+if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'localhost:8017'
 }
-if (process.env.BUILD_MODE === 'dev') {
+if (process.env.BUILD_MODE === 'production') {
   apiRoot = 'https://trello-api-x7ww.onrender.com'
 }
 export const API_ROOT = apiRoot
