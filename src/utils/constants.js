1f3e41e22189eaa6309e031f54f8b42ffd983fa3
letiendat040904@gmail.com <letiendat040904@gmail.com>
2024-03-30 08:29:31 +0700

    deploy- reconfig buildMode

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,10 @@
-export const API_ROOT = 'https://trello-api-x7ww.onrender.com'
+
+let apiRoot = ''
+
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'localhost:8017'
+}
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'https://trello-api-x7ww.onrender.com'
+}
+export const API_ROOT = apiRoot
